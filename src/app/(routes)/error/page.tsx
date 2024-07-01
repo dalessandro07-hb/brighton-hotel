237aadd58f94eb8389eae6359d88666b8ea02d40
Alessandro Rios <drios28@outlook.es>
2024-07-01 11:04:29 -0500

    fix: useSearchParams in error page without suspense

--- src/app/(routes)/error/page.tsx
+++ src/app/(routes)/error/page.tsx
@@ -2,20 +2,26 @@
 
 import MainPageContainer from '@/app/components/layout/MainPageContainer'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
-export default function ErrorPage () {
+function Search () {
   const searchParams = useSearchParams()
-
   const message = searchParams.get('message') ?? 'Ocurrió un error'
 
+  return <p className='text-xl font-medium text-destructive'>
+    {message}
+  </p>
+}
+
+export default function ErrorPage () {
   return (
     <MainPageContainer>
       <section className="flex flex-col gap-2 grow justify-center">
         <h1 className='font-bold'>Ocurrió un error:</h1>
 
-        <p className='text-xl font-medium text-destructive'>
-          {message}
-        </p>
+        <Suspense fallback={<p>Cargando...</p>}>
+          <Search />
+        </Suspense>
       </section>
     </MainPageContainer>
   )
