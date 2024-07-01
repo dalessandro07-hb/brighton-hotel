829f82cab81139940f7e9a209c35afee5a95a04d
Alessandro Rios <drios28@outlook.es>
2024-07-01 11:11:29 -0500

    fix: convert error page to server component and use props searchParams

--- src/app/(routes)/error/page.tsx
+++ src/app/(routes)/error/page.tsx
@@ -1,27 +1,20 @@
-'use client'
-
 import MainPageContainer from '@/app/components/layout/MainPageContainer'
-import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
 
-function Search () {
-  const searchParams = useSearchParams()
-  const message = searchParams.get('message') ?? 'Ocurrió un error'
+export default function ErrorPage ({ searchParams }: {
+  searchParams: {
+    message?: string
+  }
+}) {
+  const message = String(searchParams.message) ?? 'Ocurrió un error'
 
-  return <p className='text-xl font-medium text-destructive'>
-    {message}
-  </p>
-}
-
-export default function ErrorPage () {
   return (
     <MainPageContainer>
       <section className="flex flex-col gap-2 grow justify-center">
         <h1 className='font-bold'>Ocurrió un error:</h1>
 
-        <Suspense fallback={<p>Cargando...</p>}>
-          <Search />
-        </Suspense>
+        <p className='text-xl font-medium text-destructive'>
+          {message}
+        </p>
       </section>
     </MainPageContainer>
   )
